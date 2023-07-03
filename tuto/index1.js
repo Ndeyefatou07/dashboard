3aa47e8341655c1f2667c81a6e4f1019f34b4726
Ndeye Fatou Niang <ndeyefatoun126@gmail.com>
2023-07-03 19:18:23 +0000

    tableau save

--- tuto/index1.js
+++ tuto/index1.js
@@ -7,6 +7,24 @@ const toggle=document.querySelector(".toggle");
 
 var closeBtn=document.querySelector(".close-icon");
 
+const getData=()=>{
+    tableData.innerHTML= "";
+    users.forEach((data,index)=>{
+        console.log(data);
+        tableData.innerHTML += `
+        <tr index='${index}'>
+        <td>${data.nom}</td>
+        <td>${data.prenom}</td>
+        <td>${data.email}</td>
+        <td>${data.tel}</td>
+        <td>
+            <button><i class="fas fa-eye"></i></button>
+            <button style="background-color: red;"><i class="fas fa-trash"></i></button>
+        </td>
+    </tr>
+     `
+    })
+}
 
 
 // const searchBtn=body.querySelector(".search-box");
@@ -34,8 +52,65 @@ closeBtn.addEventListener("click",()=>{
     
 
 });
-var enregistrerbtn=document.querySelector(".enregistrer");
-enregistrerbtn
+// recupere les données dans le local storage 
+
+var users = JSON.parse(localStorage.getItem('users')) || [];
+// s getData();electionne le bouton enregitrer
+
+var enregistrerBtn = document.querySelector("#enregistrer");
+var formRegistre=document.querySelector("#Registre");
+enregistrerBtn.onclick = function(e) {
+    e.preventDefault();
+    enregistrerData();
+    getData();
+    formRegistre.reset('');
+    closeBtn.click();
+   
+};
+function enregistrerData() {
+    // Recuperations des informations
+    var nom = document.querySelector("#nom").value;
+    var prenom = document.querySelector("#prenom").value;
+    var email = document.querySelector("#email").value;
+    var tel = document.querySelector("#tel").value;
+
+    // Vérifier si les champs sont vides
+    if (nom === '' || prenom === '' || email === '' || tel === '') {
+        alert("Veuillez remplir tous les champs avant d'enregistrer !");
+        return;
+    }
+
+    // Créer un objet utilisateur avec les valeurs des champs de saisie
+    // var user = {
+    //     nom: nom.value,
+    //     prenom: prenom.value,
+    //     email: email.value,
+    //     tel: tel.value
+    // };
+  
+  // Ajouter le nouvel utilisateur à la liste
+  users.push({
+    nom: nom,
+    prenom: prenom,
+    email: email,
+    tel: tel
+  });
+
+  // Stocker la liste des utilisateurs mise à jour dans le local storage
+  localStorage.setItem('users', JSON.stringify(users));
+ 
+
+  // Afficher une confirmation ou effectuer toute autre action nécessaire
+  alert("Les informations ont été enregistrées avec succès !");
+
+  // Afficher les données enregistrées dans la console
+  
+  getData();
+}
+
+var tableData=document.querySelector("#userTable");
+
+
 
 
 
