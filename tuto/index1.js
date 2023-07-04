5926825b11247bfd02eb72c276fde14528dbb1c0
Ndeye Fatou Niang <ndeyefatoun126@gmail.com>
2023-07-04 18:28:42 +0000

    ajout image

--- tuto/index1.js
+++ tuto/index1.js
@@ -7,24 +7,6 @@ const toggle=document.querySelector(".toggle");
 
 var closeBtn=document.querySelector(".close-icon");
 
-const getData=()=>{
-    tableData.innerHTML= "";
-    users.forEach((data,index)=>{
-        console.log(data);
-        tableData.innerHTML += `
-        <tr index='${index}'>
-        <td>${data.nom}</td>
-        <td>${data.prenom}</td>
-        <td>${data.email}</td>
-        <td>${data.tel}</td>
-        <td>
-            <button><i class="fas fa-eye"></i></button>
-            <button style="background-color: red;"><i class="fas fa-trash"></i></button>
-        </td>
-    </tr>
-     `
-    })
-}
 
 
 // const searchBtn=body.querySelector(".search-box");
@@ -59,6 +41,8 @@ var users = JSON.parse(localStorage.getItem('users')) || [];
 
 var enregistrerBtn = document.querySelector("#enregistrer");
 var formRegistre=document.querySelector("#Registre");
+
+
 enregistrerBtn.onclick = function(e) {
     e.preventDefault();
     enregistrerData();
@@ -68,12 +52,12 @@ enregistrerBtn.onclick = function(e) {
    
 };
 function enregistrerData() {
-    // Recuperations des informations
-    var nom = document.querySelector("#nom").value;
-    var prenom = document.querySelector("#prenom").value;
-    var email = document.querySelector("#email").value;
-    var tel = document.querySelector("#tel").value;
-
+// Recuperations des informations
+var nom = document.querySelector("#nom").value;
+var prenom = document.querySelector("#prenom").value;
+var email = document.querySelector("#email").value;
+var tel = document.querySelector("#tel").value;
+var imgUrl;
     // Vérifier si les champs sont vides
     if (nom === '' || prenom === '' || email === '' || tel === '') {
         alert("Veuillez remplir tous les champs avant d'enregistrer !");
@@ -81,20 +65,16 @@ function enregistrerData() {
     }
 
     // Créer un objet utilisateur avec les valeurs des champs de saisie
-    // var user = {
-    //     nom: nom.value,
-    //     prenom: prenom.value,
-    //     email: email.value,
-    //     tel: tel.value
-    // };
+    var user = {
+        nom: nom,
+        prenom: prenom,
+        email: email,
+        tel: tel,
+        profilPic: "../tuto/avatar.png" 
+    };
   
   // Ajouter le nouvel utilisateur à la liste
-  users.push({
-    nom: nom,
-    prenom: prenom,
-    email: email,
-    tel: tel
-  });
+  users.push(user);
 
   // Stocker la liste des utilisateurs mise à jour dans le local storage
   localStorage.setItem('users', JSON.stringify(users));
@@ -104,11 +84,52 @@ function enregistrerData() {
   alert("Les informations ont été enregistrées avec succès !");
 
   // Afficher les données enregistrées dans la console
-  
   getData();
 }
-
 var tableData=document.querySelector("#userTable");
+
+const getData=()=>{
+    tableData.innerHTML= "";
+    users.forEach((data,index)=>{
+        console.log(data);
+        tableData.innerHTML += `
+        <tr index='${index}'>
+        <td> <img src="${data.profilPic}" width="40" height="40"></td>
+        <td>${data.nom}</td>
+        <td>${data.prenom}</td>
+        <td>${data.email}</td>
+        <td>${data.tel}</td>
+        <td>
+            <button><i class="fas fa-eye"></i></button>
+            <button style="background-color: red;"><i class="fas fa-trash"></i></button>
+        </td>
+    </tr>
+     `
+    })
+}
+
+// ajout image
+var profile_pic=document.querySelector("#profile-pic");
+var uploadFile=document.querySelector("#upload-field");
+uploadFile.onchange=function(){
+    if(uploadFile.files[0].sizes<5000000){
+        var fileRead = new FileReader();
+        fileRead.onload = function(e) {
+            // Le contenu du fichier a été lu avec succès
+            // Vous pouvez accéder à la URL de données via 'fileReader.result'
+            // Par exemple, pour afficher l'image :
+            imgUrl=e.target.result;
+            profile_pic.src = imgUrl;
+            console.log(imgUrl);
+        };
+        fileRead.readAsDataURL(uploadFile.files[0]);
+    }else{
+        alert("fichier Trop grand")
+    }
+}
+ 
+
+
  
 
 
